10f91be038e2a60168fab46edae9c41e3fce7824
Fernando Monteiro <ferjmonteiro@gmail.com>
2023-06-22 19:39:59 -0300

    req 1 corrigido

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,20 +1,20 @@
 from src.models.ingredient import (
-    Ingredient, Restriction
+    Ingredient,
+    Restriction
     )  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    ing_1 = Ingredient('farinha')
-    ing_2 = Ingredient('carne')
-    ing_3 = Ingredient('farinha')
+    ing_1 = Ingredient('carne')
+    ing_2 = Ingredient('manteiga')
+    ing_3 = Ingredient('carne')
 
-    assert ing_1._repr_() == "Ingredient('farinha')"
+    assert ing_1.name == 'carne'
+    assert ing_2.name != 'farinha'
     assert ing_1._hash() == ing_3.hash_()
     assert ing_1._hash() != ing_2.hash_()
-
-    assert ing_1.name == 'farinha'
-    assert ing_2.name != 'frango'
+    assert ing_1._repr_() == "Ingredient('carne')"
     assert ing_1._eq_(ing_3)
-
-    assert ing_1.restrictions == {Restriction.GLUTEN}
+    assert ing_1.restrictions == {Restriction.ANIMAL_MEAT,
+                                  Restriction.ANIMAL_DERIVED}
