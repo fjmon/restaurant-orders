dfb9ce533b4eb93719ef1fd3f8956e257bd443a9
Fernando Monteiro <ferjmonteiro@gmail.com>
2023-06-23 16:00:01 -0300

    req 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,22 @@
-# Req 3
+import csv
+from models.dish import Dish, Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        with open(source_path) as file:
+            csv_file = csv.reader(file)
+            header, *data = csv_file
+            all_dish_set = {}
+            for line in data:
+                dish = line[0]
+                price = float(line[1])
+                ingredient_type = line[2]
+                quantity = int(line[3])
+                if dish not in all_dish_set:
+                    all_dish_set[dish] = Dish(dish, price)
+                dish = all_dish_set[dish]
+                ingredient = Ingredient(ingredient_type)
+                dish.add_ingredient_dependency(ingredient, quantity)
+
+            self.dishes = set(all_dish_set.values())
