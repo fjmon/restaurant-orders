42c52ccf76112930da7fcae01253c2cf1e48ef7b
Fernando Monteiro <ferjmonteiro@gmail.com>
2023-06-23 15:43:50 -0300

    req 2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,27 @@
+import pytest
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction
+    )
 
 
-# Req 2
 def test_dish():
-    pass
+    pizza = Dish("pizza", 14.50)
+    muqueca_camarao = Dish('muqueca_camarao', 35.00)
+    pizza.add_ingredient_dependency(Ingredient('queijo mussarela'), 5)
+
+    assert pizza.name == 'pizza'
+    assert pizza == pizza
+    assert hash(pizza) == hash(repr(pizza))
+    assert pizza != muqueca_camarao
+    assert repr(pizza) == "Dish('pizza', R$14.50)"
+    assert pizza.get_ingredients() == {Ingredient('queijo mussarela')}
+    assert pizza.get_restrictions() == {
+        Restriction.ANIMAL_DERIVED, Restriction.LACTOSE
+        }
+
+    with pytest.raises(TypeError):
+        Dish('name', 'price')
+    with pytest.raises(ValueError):
+        Dish('name', 0)
